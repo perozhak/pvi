d5da1d53669e58a283f46dfeb90209ab5304517a
perozhak <vitalii.perozhak@gmail.com>
2021-03-05 01:21:11 +0200

    lab3 v.1.2(clear com and validation js)

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
 document.body.onload = start;
+
+
+
 var submitClicked = (event) => {
 
-    var username = document.getElementById("username");
-    var passw = document.getElementById("passw");
-    var age = document.getElementById("age");
-    var sex = document.querySelector("input[name='sex']:checked");
     var additionalInfo = document.getElementById("addInfo");
+    if (additionalInfo.value == "") { if (!window.confirm("You really don't want do add some info??")) event.preventDefault(); }
 
-    alert(`Hello, ${username.value} your gender is ${sex.value}
-You are ${age.value} year old, your password is ${passw.value} 
-${additionalInfo.value? "Some info:" + additionalInfo.value  : " "}`);
-
-    var data = {
-        'username': username.value,
-        'passw': passw.value,
-        'age': age.value,
-        'sex': sex.value,
-        'additionalInfo': additionalInfo.value
-
-    }
-
-    console.log(data);
-    console.log(JSON.stringify(data));
-    event.preventDefault();
 }
 
 
+
 function start() {
-
-
-    // let wk = document.getElementsByClassName("wk");
-    // let wp = document.getElementsByClassName("wp");
-    // let bb = document.getElementsByClassName("bb");
-    // let wr = document.getElementsByClassName("wr");
-    // let wb = document.getElementsByClassName("wb");
-    // let bk = document.getElementsByClassName("bk");
-    // let br = document.getElementsByClassName("br");
-    // let bp = document.getElementsByClassName("bp");
 
     let dataCounts = [];
     let dataHistory = [];
@@ -61,10 +36,9 @@ function start() {
 
     var Ctx = document.getElementById('chart');
     var Chart1 = new Chart(Ctx, {
-        // The type of chart we want to create
+
         type: 'bar',
 
-        // The data for our dataset
         data: {
             labels: [...dataHistory],
             datasets: [{
@@ -72,15 +46,10 @@ function start() {
                 borderColor: 'limegreen',
                 backgroundColor: "rgba(118, 150, 86, 0.5)",
                 cublicInterpolationMode: 'monotone',
-                //categoryPercentage: 1.0,
-                //barPercentage: 1.0,
-
-                //steppedLine: 'middle',
                 data: [...dataCounts, 0]
             }]
         },
 
-        // Configuration options go here
         options: {
 
         }
@@ -95,18 +64,17 @@ function start() {
     let hours = document.getElementById("hours");
     let minutes = document.getElementById("minutes");
     let seconds = document.getElementById("seconds");
-    let milisec = document.getElementById("milisec");
 
-    console.log(timer)
     var interval;
     let h = 0,
         ms = 0,
         m = 0,
         s = 0;
+
     startButton.addEventListener("click", (e) => {
         if (!statusClicked) {
             let st_time = Date.now()
-                // milisec.innerHTML = "00";
+
             hours.innerHTML = "00";
             minutes.innerHTML = "00";
             seconds.innerHTML = "00";
@@ -114,7 +82,7 @@ function start() {
             timer.style.color = "#769656"
             statusClicked = !statusClicked
             startButton.innerHTML = "END";
-            // let ms_prev = Date.now();
+
 
 
             interval = setInterval(() => {
@@ -126,16 +94,11 @@ function start() {
                 while (m > 60) m -= 60;
 
 
-                //milisec.innerHTML = `${(ms.toFixed(0)).length === 1 ? ('0' + ms) : ms}`;
                 seconds.innerHTML = `${(s.toFixed(0)).length === 1 ? ('0' + s) : s}`;
                 minutes.innerHTML = `${(m.toFixed(0)).length === 1 ? ('0' + m) : m}`;
                 hours.innerHTML = `${(h.toFixed(0)).length === 1 ? ('0' + h) : h}`;
 
             }, 10)
-
-
-
-
 
         } else {
             statusClicked = !statusClicked
@@ -148,11 +111,5 @@ function start() {
             s = 0;
         }
     })
-
-
-
-
-
-
 
 }
